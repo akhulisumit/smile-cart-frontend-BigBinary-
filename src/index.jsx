1fa0c387290e0eb0a96be2109a9c223b4eab9288
Sumit Akhuli <sumitakhuli5022@gmail.com>
2026-02-05 20:43:58 +0530

    Added routing functionality

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 
 import initializeAxios from "./apis/axios";
 import App from "./App";
@@ -11,7 +12,9 @@ initializeAxios();
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
