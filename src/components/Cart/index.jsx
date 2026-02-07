28869965da8f8e0459f025d30e81fd803f8430a7
Sumit Akhuli <sumitakhuli5022@gmail.com>
2026-02-07 23:10:00 +0530

    Created React query custom hook to implement parallel queries

--- src/components/Cart/index.jsx
+++ src/components/Cart/index.jsx
@@ -1,66 +1,24 @@
-import { useEffect, useState } from "react";
-
-import productsApi from "apis/products";
 import Header from "components/commons/Header";
 import NoData from "components/commons/NoData";
 import PageLoader from "components/commons/PageLoader";
 import { cartTotalOf } from "components/utils";
+import { useFetchCartProducts } from "hooks/reactQuery/useProductsApi";
 import i18n from "i18next";
-import { Toastr } from "neetoui";
-import { keys, isEmpty } from "ramda";
+import { isEmpty, keys } from "ramda";
 import useCartItemsStore from "stores/useCartItemsStore";
 import withTitle from "utils/withTitle";
 
 import PriceCard from "./PriceCard";
 import ProductCard from "./ProductCard";
 
-import { OFFER_PRICE, MRP } from "../constants";
+import { MRP, OFFER_PRICE } from "../constants";
 
 const Cart = () => {
-  const [products, setProducts] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
-  const { cartItems, setSelectedQuantity } = useCartItemsStore.pick();
+  const slugs = useCartItemsStore(store => keys(store.cartItems));
+  const { data: products = [], isLoading } = useFetchCartProducts(slugs);
 
   const totalOfferPrice = cartTotalOf(products, OFFER_PRICE);
   const totalMrp = cartTotalOf(products, MRP);
-
-  useEffect(() => {
-    fetchCartProducts();
-  }, [cartItems]);
-
-  const slugs = keys(cartItems);
-
-  const fetchCartProducts = async () => {
-    try {
-      const responses = await Promise.all(
-        slugs.map(slug => productsApi.show(slug))
-      );
-
-      setProducts(responses);
-
-      responses.forEach(({ availableQuantity, name, slug }) => {
-        if (availableQuantity >= cartItems[slug]) return;
-
-        setSelectedQuantity(slug, availableQuantity);
-        if (availableQuantity === 0) {
-          Toastr.error(
-            `${name} is no longer available and has been removed from cart`,
-            {
-              autoClose: 2000,
-            }
-          );
-        }
-      });
-    } catch (error) {
-      console.log("An error occurred:", error);
-    } finally {
-      setIsLoading(false);
-    }
-  };
-
-  useEffect(() => {
-    fetchCartProducts();
-  }, []);
 
   if (isLoading) return <PageLoader />;
 
