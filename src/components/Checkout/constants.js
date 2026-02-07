ee9d29ecefa58041729bef916c7b64ce07cca1c8
Sumit Akhuli <sumitakhuli5022@gmail.com>
2026-02-08 02:54:22 +0530

    Implemented yup validation for checkout form

--- src/components/Checkout/constants.js
+++ src/components/Checkout/constants.js
@@ -1,3 +1,13 @@
+import { t } from "i18next";
+import * as yup from "yup";
+
+export const CHECKOUT_FORM_VALIDATION_SCHEMA = yup.object().shape({
+  email: yup
+    .string()
+    .email(t("validations.emailInvalid"))
+    .required(t("validations.emailRequired")),
+});
+
 export const CHECKOUT_FORM_INITIAL_VALUES = {
   email: "",
   country: { code: "US", name: "United States" },
